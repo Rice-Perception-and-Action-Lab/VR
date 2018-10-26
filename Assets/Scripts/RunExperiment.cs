1d3c223e7f635070c2fc3be1764b642b868c0012
Courtney N Gardner <cng3@ad.rice.edu>
2018-10-26 14:07:44 -0500

    fixed timing inconsistency

--- Assets/Scripts/RunExperiment.cs
+++ Assets/Scripts/RunExperiment.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using UnityEngine;
+
+// https://docs.unity3d.com/ScriptReference/Time-frameCount.html
 
 public class RunExperiment : MonoBehaviour {
 
     public SaveData dataManager;        // The GameObject responsible for tracking trial responses
-    public Transform target;            // Position of the target (i.e., the Vive camera rig)
+    public Transform viveCamera;        // Position of the target (i.e., the Vive camera rig)
 
-    private string input_file = "U:\\vr_experiment\\VR\\Assets\\TrialData\\sample_input.json";          // A JSON file holding the information for every trial to be run
+    // The Config options
+    private Config config;              // The configuration file specifying certain experiment-wide parameters
+    private string inputFile;           // A JSON file holding the information for every trial to be run
+    private bool targetCamera;          // A boolean to determine if the object follows the user's head or not
+
     private Trial[] trials;             // The input file converted to an array of Trial objects 
     private int curTrial;               // Track the number of the current trial being run
+    private Vector3 targetPos;           // The target that the moving object aims for
     private float trialStart;           // Track the time that the current trial began
     private bool waiting;               // Boolean to track whether we're waiting between trials
     private float waitTime;             // Timer to track how long we've been waiting
@@ -25,6 +33,47 @@ public class RunExperiment : MonoBehaviour {
     private bool startTimer;
     private bool printTime;
 
+    private System.Diagnostics.Stopwatch watch;
+
+    public static class WinApi
+    {
+        /// <summary>TimeBeginPeriod(). See the Windows API documentation for details.</summary>
+
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1401:PInvokesShouldNotBeVisible"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2118:ReviewSuppressUnmanagedCodeSecurityUsage"), SuppressUnmanagedCodeSecurity]
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod", SetLastError = true)]
+
+        public static extern uint TimeBeginPeriod(uint uMilliseconds);
+
+        /// <summary>TimeEndPeriod(). See the Windows API documentation for details.</summary>
+
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1401:PInvokesShouldNotBeVisible"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2118:ReviewSuppressUnmanagedCodeSecurityUsage"), SuppressUnmanagedCodeSecurity]
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod", SetLastError = true)]
+
+        public static extern uint TimeEndPeriod(uint uMilliseconds);
+    }
+
+    [System.Serializable]
+    public class Config
+    {
+        public string dataFile;
+        public bool targetCamera;
+    }
+
+    public Config LoadConfig(string configFile)
+    {
+        try
+        {
+            string filepath = configFile.Replace(".json", "");
+            TextAsset jsonString = Resources.Load<TextAsset>(filepath);
+            Config config = JsonUtility.FromJson<Config>(jsonString.ToString());
+            return config;
+        }
+        catch (System.Exception e)
+        {
+            print("Exception: " + e.Message);
+            return null;
+        }
+    }
 
     [System.Serializable]
     class TrialArray
@@ -60,9 +109,6 @@ public class RunExperiment : MonoBehaviour {
             // Initialize the TrialData array to be the correct size in 
             // the experiment's data manager
             dataManager.InitDataArray(trialData.trials.Length, time);
-
-            Debug.Log("len of trials is " + trialData.trials.Length);
-
             return trialData.trials;
         }
         catch (System.Exception e)
@@ -83,11 +129,19 @@ public class RunExperiment : MonoBehaviour {
         {
             Debug.Log("Initializing trial " + curTrial);
 
+            watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            // Set the target for this trial (will be updated each iteration of Update function if targetCamera is true)
+            targetPos = new Vector3(viveCamera.position.x, viveCamera.position.y, viveCamera.position.z);
+      
+
             // Get the current trial from the data array
             Trial trial = trials[curTrial];
 
             // Set the inital position of the moving object
-            startPos = new Vector3(0, 3, trial.finalDist);
+            startPos = new Vector3(targetPos.x, targetPos.y, trial.finalDist);
+            Vector3 endPos = new Vector3(targetPos.x, targetPos.y, targetPos.z);
 
             // Randomly choose the shape of the object to present
             System.Random r = new System.Random();
@@ -95,7 +149,7 @@ public class RunExperiment : MonoBehaviour {
             objName = trial.objects[i];
 
             // Set the object prefab that will be displayed
-            GameObject newObj = Resources.Load(objName) as GameObject;
+            GameObject newObj = Resources.Load("Objects\\" + objName) as GameObject;
             obj = newObj.transform;
 
             // Instantiate the object so that it's visible
@@ -104,9 +158,9 @@ public class RunExperiment : MonoBehaviour {
             // Set the start time of this trial so that it can be 
             // recorded by the data manager
             trialStart = Time.time;
-            Debug.Log("Trial " + curTrial + " starting at time " + trialStart);
-
             curTrial++;
+
+            StartCoroutine(MoveOverTime(endPos, (trial.finalDist / trial.velocity)));
         }
         else
         {
@@ -167,8 +221,19 @@ public class RunExperiment : MonoBehaviour {
      */
     void Start()
     {
+        WinApi.TimeBeginPeriod(1);
+
+        // Load the config file
+        this.config = LoadConfig("config.json");
+
+        // Set the configurations based on the config file
+        inputFile = config.dataFile;
+        targetCamera = config.targetCamera;
+
+
+
         // Load the data from the desired input file
-        this.trials = LoadTrialData(input_file, Time.time);
+        this.trials = LoadTrialData(inputFile, Time.time);
 
         curTrial = 0;
         waiting = true;
@@ -181,17 +246,108 @@ public class RunExperiment : MonoBehaviour {
         printTime = true;
     }
 
+    public IEnumerator MoveOverTime(Vector3 finalPos, float seconds)
+    {
+        float elapsedTime = 0.0f;
+        float trialTTC = 0.0f;
+        bool objHidden = false;
+        Vector3 startingPos = movingObj.position;
+        Debug.Log("SECONDS: " + seconds);
+        while (elapsedTime < seconds)
+        {
+            if (!objHidden && elapsedTime >= trials[curTrial - 1].timeVisible)
+            {
+                Debug.Log("TIME VISIBLE: " + elapsedTime);
+                Debug.Log("POSITION: " + movingObj.position.x + " " + movingObj.position.y + " " + movingObj.position.z);
+                HideObj();
+                objHidden = true;
+            }
+
+            movingObj.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            if (objHidden)
+                trialTTC += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        movingObj.position = finalPos;
+        Debug.Log("ELAPSED TIME: " + elapsedTime);
+        Debug.Log("TTC: " + trialTTC);
+    }
+
     /**
      * Every frame, move the object and check if it should disappear 
      * from view. If the first trial hasn't been initialized yet, then
      * do so.
      */
-     void Update()
+    void FixedUpdate()
     {
         // Make sure there is currently a trial running before attempting
         // to move an object
         if (movingObj)
         {
+            // Update the target position if we're trying to follow the user's head
+            if (targetCamera)
+            {
+                targetPos = viveCamera.position;
+            }
+
+            if (startTimer)
+            {
+                ttcTimer += Time.deltaTime;
+                float temp = Time.time;
+                timer2 += Time.time - prevTime;
+                prevTime = temp;
+            }
+
+            Vector3 newPos = movingObj.position;
+
+            // If the object has been visible for the time visible param, hide it
+            
+        }
+        else
+        {
+            // Don't check for a timeout if we're waiting to end the trial
+            if (waiting)
+            {
+                // Only initialize a new trial after the waiting period is over
+                if (waitTime >= 3.0f)
+                {
+                    waiting = false;
+                    InitializeTrial();
+                }
+                else
+                {
+                    // Increment wait time
+                    waitTime += Time.deltaTime;
+                }
+            }
+            else
+            {
+                // Check for a timeout (i.e., no controller response for some specified amount of time)
+                if ((curTrial - 1 < trials.Length) && (Time.time - trialStart) >= (trials[curTrial - 1].timeVisible * 2))
+                {
+                    Debug.Log("Timeout");
+                    CompleteTrial(Time.time, false);
+                }
+            }
+        }
+    }
+
+
+    /*void FixedUpdate()
+    {
+        Debug.Log("Frame Count: " + Time.frameCount);
+
+        // Make sure there is currently a trial running before attempting
+        // to move an object
+        if (movingObj)
+        {
+            // Update the target position if we're trying to follow the user's head
+            if (targetCamera)
+            {
+                targetPos = viveCamera.position;
+            }
+
             // step size equals current trial's velocity * frame rate
             float step = trials[curTrial - 1].velocity * Time.deltaTime;
 
@@ -201,13 +357,11 @@ public class RunExperiment : MonoBehaviour {
             //Vector3 adj = new Vector3(0.0f, 0.0f, (movingObj.localScale.z / 2));
             //Vector3 adj = new Vector3(0.0f, (movingObj.localScale.y / 2), 0.0f);
             Vector3 adj = new Vector3(0.0f, 0.0f, (movingObj.localScale.z / 2.0f));
+            //Vector3 adj = new Vector3(0.0f, 0.0f, (movingObj.localScale.z));
+            //Debug.Log("adj z: " + adj.z);
 
-            Debug.Log("ADJ: " + adj.z);
-
-            movingObj.position = Vector3.MoveTowards(movingObj.position, target.position + adj, step);
-
-            Debug.Log("Target Position X: " + target.position.x + " Y: " + target.position.y + " Z: " + target.position.z);
-            Debug.Log("Moving Obj Size X: " + movingObj.localScale.x + " Y: " + movingObj.localScale.y + " Z: " + movingObj.localScale.z);
+            //movingObj.position = Vector3.MoveTowards(movingObj.position, targetPos, step);
+           movingObj.position = Vector3.MoveTowards(movingObj.position, targetPos - adj, step);
 
             if (startTimer)
             {
@@ -226,6 +380,8 @@ public class RunExperiment : MonoBehaviour {
                 Debug.Log("Contact");
                 Debug.Log("TTC TIMER: " + ttcTimer);
                 Debug.Log("TIMER 2: " + timer2);
+                watch.Stop();
+                Debug.Log("STOPWATCH: " + watch.ElapsedMilliseconds);
                 startTimer = false;
                 printTime = false;
             }
@@ -262,7 +418,7 @@ public class RunExperiment : MonoBehaviour {
                     CompleteTrial(Time.time, false);
                 }
             }
-        }
-    }
+        }*/
+    //}
 
 }
