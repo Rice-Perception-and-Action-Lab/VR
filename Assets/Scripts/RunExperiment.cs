8da039e7402340f97bc56e006f35d8b17632335b
Courtney N Gardner <cng3@ad.rice.edu>
2019-01-25 11:56:56 -0600

    changed feedback message rounding

--- Assets/Scripts/RunExperiment.cs
+++ Assets/Scripts/RunExperiment.cs
@@ -175,22 +175,9 @@ public class RunExperiment : MonoBehaviour {
                 feedbackMsg.text = "Experiment complete";
                 dataManager.Save();
                 curTrial++;
-                //ExitExperiment();
             }
         }
     }
-
-    /*public void ExitExperiment()
-    {
-        // save any game data here
-        #if UNITY_EDITOR
-             // Application.Quit() does not work in the editor so
-             // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-             UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
-    }*/
 
 
     public void HideObj()
