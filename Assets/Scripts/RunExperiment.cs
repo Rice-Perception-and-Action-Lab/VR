73c11ea9d410d1da131cf21c5e49124355eedb34
Stephanie T Xie <stx1@ad.rice.edu>
2019-11-21 14:28:42 -0600

    changed .txt to .cus for custom motion files

--- Assets/Scripts/RunExperiment.cs
+++ Assets/Scripts/RunExperiment.cs
@@ -47,8 +47,8 @@ public class RunExperiment : MonoBehaviour {
     private float stepSize;                 // The fraction that an object moves on every call of the MoveObjsByStep method; based on the target frame rate
     private float hideTime;
     private string posString;
-    private float ttcActual;                //the theoretical TTC calculated from startPos, time visible, and velocity
-    private float ttcActualSim;             //the TTC calculated by the simulator when the endPos is 0,0,0
+    private float ttcActual;                //the theoretical TTC calculated from startPos, time visible, and velocity, for objects along the z-axis (used to calculate feedback msg)
+    private float ttcActualSim;             //the TTC calculated by the simulator when the object reaches its endpoint
     private float estimate;                 //the calculation of the participant's TTC estimate NOTE: 1 object PM Scenes only
     private float timeVisible;              //the time the object is visible NOTE: 1 object PM Scenes only
     public bool pmVisible;                  //True if the object is still visible on the screen. Used in TrackControllerResponse
